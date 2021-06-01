54cc2ef974c9f81450cf43f0eb867aaa0ab39bc0
Vladislav Kulachko <vladcoool@gmail.com>
2021-06-01 10:09:30 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -11,50 +11,50 @@ inputNumbOfBoxes.addEventListener("input", (event) => {
 
 //=================================================================================
 
-function createBoxes(numbOfBoxes) {
-  let newBox = " ";
-  let width = 30;
-  let height = 30;
-  const randColor = () => Math.floor(Math.random() * 255);
-  for (let i = 0; i < numbOfBoxes; i += 1) {
-    width += 10;
-    height += 10;
-    const r = randColor();
-    const g = randColor();
-    const b = randColor();
-    newBox += `<div style = "width: ${width}px; height: ${height}px;
-      background: rgb(${r}, ${g}, ${b});" ></div>`;
-  }
-  return newBox;
-}
-
-btnCreateBoxes.addEventListener("click", () =>
-  boxes.insertAdjacentHTML("beforeend", createBoxes(amount))
-);
-
-//--------------------------------------------------------------------------------
-
 // function createBoxes(numbOfBoxes) {
+//   let newBox = " ";
 //   let width = 30;
 //   let height = 30;
 //   const randColor = () => Math.floor(Math.random() * 255);
 //   for (let i = 0; i < numbOfBoxes; i += 1) {
 //     width += 10;
 //     height += 10;
-//     const newBox = document.createElement("div");
-//     newBox.style.width = `${width}px`;
-//     newBox.style.height = `${height}px`;
 //     const r = randColor();
 //     const g = randColor();
 //     const b = randColor();
-//     newBox.style.backgroundColor = `rgb(${r}, ${g}, ${b}`;
-//     boxes.append(newBox);
-
-//     console.log(boxes.children);
+//     newBox += `<div style = "width: ${width}px; height: ${height}px;
+//       background: rgb(${r}, ${g}, ${b});" ></div>`;
 //   }
+//   return newBox;
 // }
 
-// btnCreateBoxes.addEventListener("click", () => createBoxes(amount));
+// btnCreateBoxes.addEventListener("click", () =>
+//   boxes.insertAdjacentHTML("beforeend", createBoxes(amount))
+// );
+
+//--------------------------------------------------------------------------------
+
+function createBoxes(numbOfBoxes) {
+  let width = 30;
+  let height = 30;
+  const randColor = () => Math.floor(Math.random() * 255);
+  for (let i = 0; i < numbOfBoxes; i += 1) {
+    width += 10;
+    height += 10;
+    const newBox = document.createElement("div");
+    newBox.style.width = `${width}px`;
+    newBox.style.height = `${height}px`;
+    const r = randColor();
+    const g = randColor();
+    const b = randColor();
+    newBox.style.backgroundColor = `rgb(${r}, ${g}, ${b}`;
+    boxes.appendChild(newBox);
+
+    console.log(boxes.children);
+  }
+}
+
+btnCreateBoxes.addEventListener("click", () => createBoxes(amount));
 
 //--------------------------------------------------------------------------------
 
